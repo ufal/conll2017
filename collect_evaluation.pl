278954a3ec01b2490c9586d43663eec8e8ef7751
Dan Zeman <zeman@ufal.mff.cuni.cz>
2017-05-28 22:09:11 +0200

    Rank treebanks by CLAS.

--- collect_evaluation.pl
+++ collect_evaluation.pl
@@ -268,6 +268,23 @@ elsif ($metric eq 'ranktreebanks')
         printf("%2d.   %s   %5.2f   %s   %5.2f   ±%5.2f\n", $i, $tbk, $treebanks->{$key}{'max-LAS-F1'}, $team, $treebanks->{$key}{'avg-LAS-F1'}, sqrt($treebanks->{$key}{'var-LAS-F1'}));
     }
 }
+elsif ($metric eq 'ranktreebanks-CLAS')
+{
+    my $treebanks = rank_treebanks(\@alltbk, \@results, 'CLAS-F1');
+    my @keys = sort {$treebanks->{$b}{'max-CLAS-F1'} <=> $treebanks->{$a}{'max-CLAS-F1'}} (keys(%{$treebanks}));
+    my $i = 0;
+    print("                      max     maxteam    avg     stdev\n");
+    foreach my $key (@keys)
+    {
+        $i++;
+        my $tbk = $key;
+        $tbk .= ' ' x (13-length($tbk));
+        my $team = $treebanks->{$key}{'teammax-CLAS-F1'};
+        $team .= ' ' x (8-length($team));
+        #printf("%2d.   %s   %5.2f   %s   %5.2f   ±%5.2f\n", $i, $tbk, $treebanks->{$key}{'max-CLAS-F1'}, $team, $treebanks->{$key}{'avg-CLAS-F1'}, sqrt($treebanks->{$key}{'var-CLAS-F1'}));
+        printf("%2d. & %s & %5.2f & %s & %5.2f & ±%5.2f\\\\\\hline\n", $i, $tbk, $treebanks->{$key}{'max-CLAS-F1'}, $team, $treebanks->{$key}{'avg-CLAS-F1'}, sqrt($treebanks->{$key}{'var-CLAS-F1'}));
+    }
+}
 else
 {
     # Sanity check: If we compute average LAS over all treebanks we should replicate the pre-existing total-LAS-F1 score.
